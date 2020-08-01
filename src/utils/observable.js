55b3087d06a29273078d91b31244dad9dbd713f9
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2020-07-31 23:22:58 -0400

    Add argument to fireEvent

--- src/utils/observable.js
+++ src/utils/observable.js
@@ -1,7 +1,7 @@
 // One of the ideas was to track state based on fired events
 // Each new fired event creates new state
 // We can track input and output of the function as call fram of the stack
-const interceptors = new Map();
+//const interceptors = new Map();
 const states = new Map();
 
 let stateStart = 0;
@@ -18,6 +18,7 @@ export default class Observable {
     return this.off(name, f);
   }
   //TODO: Create before, after, during methods
+  /*
   intercept(name, f) {
     //ony one interecpt is allowed to eliminate race conditions
     if (interceptors.has(name)) {
@@ -25,20 +26,24 @@ export default class Observable {
     }
     interceptors.set(name, f);
   }
+ */
   off(name, f) {
     // TODO
     if (this.listeners.has(name)) {
       this.listeners.get(name);
     }
   }
-  fireEvent(name) {
+  fireEvent(name, arg=undefined) {
     if (this.listeners.has(name)) {
       for (let f of this.listeners.get(name)) {
+        /*
         if (interceptors.has(name)) {
           f(interceptors.get(name)());
         } else {
           f();
         }
+       */
+        f(arg);
       }
     }
   }
